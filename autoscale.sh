eba8f5eec45875840994600cfaa6b95492967521
nikhilpatole <nikhil.patole27@gmail.com>
2022-03-10 15:57:59 -0600

    docker swarm join --token SWMTKN-1-60jj18tjxe517gvhvn5444lxudc7vucdkhysqjxi8i1j5upbyd-62xvhg3uet3nk0er2k1ufl36x 10.0.0.8:2377

--- autoscale.sh
+++ autoscale.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-
-git clone https://github.com/komal-gogi/sample-nodejs-app
+  
+#Cloning the repository 
+git clone https\://github.com/komal-gogi/sample-nodejs-app
 cd sample-nodejs-app
+
+#Initializing Docker swarm
+docker swarm init
+echo -e '\n \n'
+
+#we can join manager node to worker using "docker swarm join --token ip"
+
+#Deploying docker swarm for single node
 docker stack deploy -c docker-compose.yml swarmnodeapp
+echo -e "\nservices created .........\n\n"
 
-
+#List the docker services
+docker service ls
